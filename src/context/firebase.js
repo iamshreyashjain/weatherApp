59ce3f312f94e6980eaddda84b6341d6c1c91582
Shreyash Jain <shreyashjain41@gmail.com>
2024-08-05 06:57:56 +0530

    2nd commit

---
+++ src/context/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAAgcqDswepf8wSncJox_w9wX2OX2Mejxw",
+  authDomain: "learner-dc009.firebaseapp.com",
+  projectId: "learner-dc009",
+  storageBucket: "learner-dc009.appspot.com",
+  messagingSenderId: "215967958133",
+  appId: "1:215967958133:web:beaf43911664a06850a43c",
+  databaseURL: 'https://learner-dc009-default-rtdb.asia-southeast1.firebasedatabase.app'
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
